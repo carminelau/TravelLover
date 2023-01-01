832e50e40fbdcdacbf7c94aaa68cf418def8fc1b
Carmine Laudato <56676908+carminelau@users.noreply.github.com>
2023-01-01 18:47:17 +0100

    FE e BE: insert login and signup

--- flask/databases.py
+++ flask/databases.py
@@ -6,3 +6,4 @@ bus_station= db.bus_stations
 train_line = db.train_lines
 bus_line = db.bus_lines
 poi_itinerary = db.poi_itinerary
+users = db.users
--- flask/server.py
+++ flask/server.py
@@ -14,6 +14,7 @@ import datetime
 import json
 import geojson
 import pandas as pd
+import re
 
 app = Flask(__name__)
 
@@ -27,6 +28,54 @@ def insertAllTrains():
     data = request.get_json()
     train_line.insert_one(data)
     return "ok"
+
+#login
+@app.route("/login", methods=['POST'])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    user = users.find_one({"username": username, "password": password})
+    if user is not None:
+        user = users.find_one({"email": username, "password": password})
+        if user is not None:
+            return jsonify({"status": "ok", "user": user})
+        else:
+            return jsonify({"status": "nessun account trovato"})
+    else:
+        return jsonify({"status": "nessun account trovato"})
+
+#registrazione con username, password, email, nome, cognome, data di nascita
+@app.route("/register", methods=['POST'])
+def register():
+    username = request.form["username"]
+    password = request.form["password"]
+    email = request.form["email"]
+    name = request.form["name"]
+    surname = request.form["surname"]
+    birthdate = request.form["birthdate"]
+    risposta = {}
+    if re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        if re.match(r"^[a-zA-Z0-9]*$", username):
+            if re.match(r"^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$", password):
+                if re.match(r"^[a-zA-Z]*$", name):
+                    if re.match(r"^[a-zA-Z]*$", surname):
+                        if re.match(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}$", birthdate):
+                            user = users.insert_one({"username": username, "password": password, "email": email, "name": name, "surname": surname, "birthdate": birthdate})
+                            risposta["status"] = "ok"
+                            risposta["user"] = user
+                        else:
+                            risposta["status"] = "errore data di nascita"
+                    else:
+                        risposta["status"] = "errore cognome"
+                else:  
+                    risposta["status"] = "errore nome"
+            else:
+                risposta["status"] = "errore password"
+        else:   
+            risposta["status"] = "errore username"
+    else:
+        risposta["status"] = "errore email"
+    return jsonify(risposta)
 
 
 if __name__ == "__main__":
