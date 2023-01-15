8acaa2d7957d86a886b0210ea8a3551be90b90c1
Carmine Laudato <56676908+carminelau@users.noreply.github.com>
2023-01-15 14:13:27 +0100

    Fix Connection DB

--- backend/configs.py
+++ backend/configs.py
@@ -2,9 +2,8 @@ from flask import Flask
 from flask_pymongo import PyMongo
 
 application = Flask( __name__)
-application.config["MONGO_URI"] = "mongodb+srv://root:BDq0yVKajvkufJoD@travellover.irvmm4y.mongodb.net/?retryWrites=true&w=majority"
+application.config["MONGO_URI"] = "mongodb+srv://root:BDq0yVKajvkufJoD@travellover.irvmm4y.mongodb.net/test"
 application.config['MONGO_CONNECT'] = True
 
 mongo = PyMongo(application)
-
 db=mongo.db
