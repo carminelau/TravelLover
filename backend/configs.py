e0772922464ddc4b756a8c8ea9416e0c2946bc64
Carmine Laudato <56676908+carminelau@users.noreply.github.com>
2023-01-15 12:34:58 +0100

    update connection String

--- backend/configs.py
+++ backend/configs.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_pymongo import PyMongo
 
 application = Flask( __name__)
-application.config["MONGO_URI"] = "mongodb://gis-code-mongo-1:27017/GIS"
+application.config["MONGO_URI"] = "mongodb+srv://root:BDq0yVKajvkufJoD@travellover.irvmm4y.mongodb.net/?retryWrites=true&w=majority"
 application.config['MONGO_CONNECT'] = True
 
 mongo = PyMongo(application)
