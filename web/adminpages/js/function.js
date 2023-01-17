1c313a6183578874d8d47ec7facd520938924434
Carmine Laudato <c.laudato2@studenti.unisa.it>
2023-01-17 11:00:49 +0100

    riparazione ajax

--- web/adminpages/js/function.js
+++ web/adminpages/js/function.js
@@ -1,7 +1,7 @@
 function visualizzaTipoCheck(tipo,azione){
         $.ajax({
             url: "http://127.0.0.1:5000/visualizzaTipo",
-            method: "GET",
+            method: "POST",
             cache: false,
             data:{
                 criterio: tipo,
@@ -14,9 +14,10 @@ function visualizzaTipoCheck(tipo,azione){
                 $(".risultato").css("margin", "0 auto");
                 $(".risultato").css("position", "relative");
                 $(".risultato").css("width", "100%");
-
-                response.map(el=>{$(".risultato").append(el)});
-                $(".risultato").append("<hr>")
+                let dataArr = ['ciao','sono', 'un', 'array'];
+                response.luoghi.forEach(function (item) {
+                    $(".risultato").append("<div class='card' style='width: 100%;'> <div class='card-body'> <h5 class='card-title'>"+item.Nome+"</h5> <h6 class='card-subtitle mb-2 text-muted'></h6> <p class='card-text'>"+item.Descrizione+"</p> <a href='#' class='card-link'>Card link</a> <a href='#' class='card-link'>Another link</a> </div> </div>");
+                });
             },
             error: function () {
                 alert("ERRORE CHIAMATA ASINCRONA");
